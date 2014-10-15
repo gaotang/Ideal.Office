edf3e7bb05234547f8992c0f85bb5ad7a34b30e9
gaotang <qq23745038@126.com>
2014-10-15 09:03:08 +0800

    添加对Excel的导入操作

---
+++ Office.NPOI.Web/Imports/ImportBase.cs
@@ -0,0 +1,92 @@
+﻿using Ideal.Office.Excel;
+using NPOI.SS.Util;
+using System.IO;
+namespace Ideal.Office.Web
+{
+    public partial class Import
+    {
+        public Excel2003 Excel03 { get; set; }
+        public Import(string filePath, int sheetIndex = 0)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                var poi = new Excel2003(fs);
+                poi.Sheet = poi.Workbook.GetSheetAt(sheetIndex);
+                this.Excel03 = poi;
+            }
+        }
+
+        /// <summary>
+        ///  获取Table某个TD合并的列数和行数等信息。与Excel中对应Cell的合并行数和列数一致。
+        /// </summary>
+        /// <param name="rowIndex">行号</param>
+        /// <param name="colIndex">列号</param>
+        /// <param name="colspan">TD中需要合并的行数</param>
+        /// <param name="rowspan">TD中需要合并的列数</param>
+        /// <param name="rowspan">此单元格是否被某个行合并包含在内。如果被包含在内，将不输出TD。</param>
+        /// <returns></returns>
+        private void GetTdMergedInfo(int rowIndex, int colIndex, out int colspan, out int rowspan, out bool isByRowMerged)
+        {
+            var sht = Excel03.Sheet;
+            colspan = 1;
+            rowspan = 1;
+            isByRowMerged = false;
+            int regionsCuont = sht.NumMergedRegions;
+            //Region region;
+            CellRangeAddress region;
+
+            for (int i = 0; i < regionsCuont; i++)
+            {
+                //region = sht.GetMergedRegionAt(i);
+                //if (region.RowFrom == rowIndex && region.ColumnFrom == colIndex)
+                //{
+                //    colspan = region.ColumnTo - region.ColumnFrom + 1;
+                //    rowspan = region.RowTo - region.RowFrom + 1;
+                //    return;
+                //}
+                //else if (rowIndex > region.RowFrom && rowIndex <= region.RowTo && colIndex >= region.ColumnFrom && colIndex <= region.ColumnTo)
+                //{
+                //    isByRowMerged = true;
+                //}
+                region = sht.GetMergedRegion(i);
+                if (region.FirstRow == rowIndex && region.FirstColumn == colIndex)
+                {
+                    colspan = region.LastColumn - region.FirstColumn + 1;
+                    rowspan = region.LastRow - region.FirstRow + 1;
+                    return;
+                }
+                else if (rowIndex > region.FirstRow && rowIndex <= region.LastRow && colIndex >= region.FirstColumn && colIndex <= region.LastColumn)
+                {
+                    isByRowMerged = true;
+                }
+            }
+        }
+
+
+        private void GetTdMergedInfo(Excel2003 poi, int rowIndex, int colIndex, out int colspan, out int rowspan, out bool isByRowMerged)
+        {
+            var sht = poi.Sheet;
+            colspan = 1;
+            rowspan = 1;
+            isByRowMerged = false;
+            int regionsCuont = sht.NumMergedRegions;
+            //Region region;
+            CellRangeAddress region;
+
+            for (int i = 0; i < regionsCuont; i++)
+            {
+                region = sht.GetMergedRegion(i);
+                if (region.FirstRow == rowIndex && region.FirstColumn == colIndex)
+                {
+                    colspan = region.LastColumn - region.FirstColumn + 1;
+                    rowspan = region.LastRow - region.FirstRow + 1;
+                    return;
+                }
+                else if (rowIndex > region.FirstRow && rowIndex <= region.LastRow && colIndex >= region.FirstColumn && colIndex <= region.LastColumn)
+                {
+                    isByRowMerged = true;
+                }
+            }
+        }
+    }
+}
--- Office.NPOI.Web/Imports/ImportByExcel.cs
+++ Office.NPOI.Web/Imports/ImportByExcel.cs
@@ -1,6 +1,61 @@
-﻿namespace Ideal.Office.Web
+﻿using Ideal.Office.Excel;
+using System.IO;
+using System.Text;
+namespace Ideal.Office.Web
 {
     public partial class Import
     {
+        public string ToJsonString(int titleIndex = 0)
+        {
+            var sht = Excel03.Sheet;
+            //取行Excel的最大行数
+            int rowsCount = sht.PhysicalNumberOfRows;
+            //为保证Table布局与Excel一样，这里应该取所有行中的最大列数（需要遍历整个Sheet）。
+            //为少一交全Excel遍历，提高性能，我们可以人为把第0行的列数调整至所有行中的最大列数。
+            int colsCount = sht.GetRow(0).PhysicalNumberOfCells;
+
+            int colSpan;
+            int rowSpan;
+            bool isByRowMerged;
+            var tempTd = "";
+
+            StringBuilder table = new StringBuilder(rowsCount * 32);
+
+            table.Append("[");
+            for (int rowIndex = titleIndex; rowIndex < rowsCount; rowIndex++)
+            {
+                tempTd = "";
+                for (int colIndex = 0; colIndex < colsCount; colIndex++)
+                {
+                    GetTdMergedInfo(rowIndex, colIndex, out colSpan, out rowSpan, out isByRowMerged);
+                    //如果已经被行合并包含进去了就不输出TD了。
+                    //注意被合并的行或列不输出的处理方式不一样，见下面一处的注释说明了列合并后不输出TD的处理方式。
+                    if (isByRowMerged)
+                    {
+                        continue;
+                    }
+
+                    tempTd += sht.GetRow(rowIndex).GetCell(colIndex);
+                    //列被合并之后此行将少输出colSpan-1个TD。
+                    if (colSpan > 1)
+                        colIndex += colSpan - 1;
+
+                    if (colSpan > 1)
+                        colIndex += colSpan - 1;
+                    tempTd += ",";
+
+                }
+                var b = tempTd.Replace(",", "");
+                if (!string.IsNullOrEmpty(b))
+                {
+                    table.Append("{");
+                    table.Append(tempTd.Trim(','));
+                    table.Append("}");
+                }
+            }
+            table.Append("]");
+
+            return table.ToString();
+        }
     }
 }
