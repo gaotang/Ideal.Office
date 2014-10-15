8bf1f297467f06fb0ea45e8e3c4da6a34c1906f5
gaotang <qq23745038@126.com>
2014-10-15 10:24:39 +0800

    添加对Excel2003和2007导出的支持。

---
+++ Office.NPOI/Excel2007/Excel2007ByContent.cs
@@ -0,0 +1,14 @@
+﻿namespace Ideal.Office.Excel
+{
+    using System.IO;
+
+    using NPOI.XSSF.UserModel;
+
+    /// <summary>
+    /// 初始化Excel2007
+    /// </summary>
+    public partial class Excel2007
+    {
+
+    }
+}
---
+++ Office.NPOI/Excel2007/Excel2007ByInit.cs
@@ -0,0 +1,19 @@
+﻿namespace Ideal.Office.Excel
+{
+    using System.IO;
+
+    using NPOI.HSSF.UserModel;
+    using NPOI.SS.UserModel;
+    using NPOI.SS.Util;
+    using NPOI.XSSF.UserModel;
+    using System.Data;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// 初始化Excel2007
+    /// </summary>
+    public partial class Excel2007
+    {
+
+    }
+}
